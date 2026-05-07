edd50c7962e26f47f00f739fd5b13357642aeafc
briandataengineering <brian.dataengineering@gmail.com>
2026-05-07 15:32:23 +0300

    Update intro.sql

--- intro.sql
+++ intro.sql
@@ -80,3 +80,78 @@ WHERE ID=2
 SELECT *
 FROM materialized_table_view
 
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+LOADING DATA FROM S3 BUCKET TO SNOWFLAKE
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+USE ROLE ACCOUNTADMIN;
+
+USE WAREHOUSE COMPUTE_WH;
+
+USE SCHEMA MY_DB.MY_SCHEMA;
+
+CREATE OR REPLACE TABLE city(
+  city_id STRING,
+  city_name STRING
+);
+
+//Creating storage integration
+CREATE OR REPLACE STORAGE INTEGRATION s3_int
+TYPE = EXTERNAL_STAGE
+STORAGE_PROVIDER= 'S3'
+ENABLED= TRUE
+STORAGE_AWS_ROLE_ARN= 'arn:aws:iam::876762732779:role/snowflakerole'
+STORAGE_ALLOWED_LOCATIONS= ('s3://snoflake-project-brian/')
+
+//Describe storage integration
+DESC INTEGRATION s3_int;
+
+//Create file format
+CREATE OR REPLACE FILE FORMAT my_csv_format
+    TYPE ='CSV'
+    FIELD_DELIMITER= ','
+    RECORD_DELIMITER= '\n';
+
+//Create external stage
+CREATE OR REPLACE STAGE my_s3_stage
+    STORAGE_INTEGRATION = s3_int
+    URL= 's3://snoflake-project-brian/'
+    FILE_FORMAT = my_csv_format;
+
+list @my_s3_stage;
+
+COPY INTO city
+FROM @my_s3_stage
+FILE_FORMAT= (FORMAT_NAME=my_csv_format)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
